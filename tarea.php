b56b11de9ff343ed70f930d2c37429d463cf7ce0
Carlos Paz <pazaupazcarlossebastian@gmail.com>
2020-02-25 19:46:06 -0500

    Iniciando proyecto

---
+++ tarea.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$arraynum=[3,4,5,7,9,78,7,6,];
+
+for($i=0;$i<count($arraynum);$i++){
+	for($j=$i+1;$j<count($arraynum);$j++){
+		if($arraynum[$j]<$arraynum[$i])
+	{
+		$tmp=$arraynum[$i];
+		$arraynum[$i]=$arraynum[$j];
+		$arraynum[$j]=$tmp;
+	}
+	 
+	
+	}
+	
+	
+}
+for($i=0;$i<count($arraynum);$i++){
+	echo $arraynum[$i]."<br>";
+}
+
+
+
+
+
+
+?>
